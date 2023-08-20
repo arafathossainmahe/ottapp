e79cd31818e7da42a503d28fd4718c8a40810936
kmsanjar47 <khan.md.saifullah@g.bracu.ac.bd>
2023-08-20 23:30:45 +0600

    initial commit

---
+++ lib/views/screens/discover_new_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class DiscoverNewPage extends StatelessWidget {
+  const DiscoverNewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
