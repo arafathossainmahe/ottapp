8fbc9cf40cc14b97560089c179475f1c86a5cb57
kmsanjar47 <khan.md.saifullah@g.bracu.ac.bd>
2023-08-21 01:04:06 +0600

    discover_new_page added

--- lib/views/screens/discover_new_page.dart
+++ lib/views/screens/discover_new_page.dart
@@ -1,9 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ottapp/views/widgets/custom_searchbar.dart';
+
 class DiscoverNewPage extends StatelessWidget {
   const DiscoverNewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.only(left: 16.0.w,right: 16.0.w),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20.h,),
+            //Discover_New
+            Text(
+              "Discover New",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.sp,
+                  fontWeight: FontWeight.bold),
+            ),
+
+            //Search_Bar
+            SizedBox(height: 20.h,),
+            const CustomSearchBar(hintText: "Search Here"),
+            //Hollywood
+            SizedBox(height: 20,),
+            Text(
+              "Hollywood",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.sp,
+                  fontWeight: FontWeight.bold),
+            ),
+
+            //WebSeries
+            SizedBox(height: 20,),
+            Text(
+              "Web Series",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.sp,
+                  fontWeight: FontWeight.bold),
+            ),
+            //Bollywood
+            SizedBox(height: 20,),
+            Text(
+              "Bollywood",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.sp,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/views/widgets/custom_searchbar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class CustomSearchBar extends StatelessWidget {
+  final String hintText;
+  const CustomSearchBar({required this.hintText,super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      height: 50.h,
+      decoration: ShapeDecoration(
+        gradient:
+        LinearGradient(colors: [Colors.black54, Colors.grey.shade800]),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5)),
+      ),
+      child: Center(
+        child: TextField(
+          cursorColor: Colors.grey,
+          style: const TextStyle(color: Colors.grey),
+          decoration: InputDecoration(
+            prefixIcon: const Icon(CupertinoIcons.search),
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.grey,fontSize: 18.sp),
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+    );
+  }
+}
